2fb9551ec20f8a4c63b2ec5eb814cb21fb3d2546
InternecionCrystal <internecioncrystal@gmail.com>
2025-08-30 01:57:04 +0630

    Smooth Camera Movement
    Camera is much smoother.
    Zoom Added
    Reset Camera Position after a time period.
    Menu and UI PreWork
    MouseLock - P
    Jump might be a bit too high
    Running bug fixed
    Error Handling on Quaternion Normalization error due to overloaded numbers
    
    Mouse Lock Properly Done with Helper Function

--- Characters/Solis&Luna/PlayerScript.gd
+++ Characters/Solis&Luna/PlayerScript.gd
@@ -5,23 +5,40 @@ class_name Kairo_Traveller
 @export var ShiftToggleRun := true
 @export var sens := 0.5
 
+#--- UI
+var InUI = false
+var HideUI = false
+# TODO: Replace with UI Classes
+@export var MenuUi: Node
+@export var ShopUi: Node
+@export var EventUi: Node
+@export var CharacterUi: Node # Character and Inventory
+
 #--- Camera Movement
 @onready var CamPivot: Node3D = $CamOrigin
+@onready var CamSpring: SpringArm3D = $CamOrigin/SpringArm3D
 @onready var Cam: Camera3D = $CamOrigin/SpringArm3D/Camera3D
+@onready var ResetCamTimer: Timer = $ResetCam
+@export var MouseLockViaShortcut := false
 @export var MouseLocked := false
 @export var Move_Dir = Vector3.ZERO
 @export var VisualNode: Node3D # Node Responsible for visualizing characters
+@export var ResetCam := true
+
 
 var x_rot = 0
 var y_rot = 0
 
+var zoomMax = 6
+var zoomMin = 0.8
+var ZoomPressure = 0.01
 
 #--- Movement
 @onready var IdleTimer: Timer = $IdleTimer
 @export var Speed = 0.0
 @export var WalkSpeed = 3.0
-@export var RunSpeed = .0
-@export var JumpVel = 4.5
+@export var RunSpeed = 8.0
+@export var JumpVel = 6.5
 @export var KnockbackVel := Vector3.ZERO
 @export var KnockbackDecel := 10.0
 @export var Sprinting := false
@@ -46,6 +63,7 @@ func handle_movement(input_dir: Vector2, delta):
 	Move_Dir = (cam_forward * -input_dir.y + cam_right * input_dir.x)
 	Move_Dir.z = Move_Dir.z
 	
+	
 	if Move_Dir:
 		velocity.x = Move_Dir.x * Speed
 		velocity.z = Move_Dir.z * Speed
@@ -58,6 +76,7 @@ func handle_movement(input_dir: Vector2, delta):
 		var target_rotation: Quaternion
 		var current_roation = VisualNode.global_transform.basis.get_rotation_quaternion()
 		target_rotation = Quaternion(Vector3.FORWARD, Move_Dir.normalized())
+		if not target_rotation.is_normalized(): return #DO NOT PROCEED FOR ERROR REDUCTION
 		
 		var new_rotation = current_roation.slerp(target_rotation, 7.2 * delta)
 		
@@ -70,12 +89,21 @@ func handle_movement(input_dir: Vector2, delta):
 func start_idleTimer():
 	IdleTimer.start(20.0)
 
+func help_mouseMode():
+	if MouseLocked:
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	else:
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+
+func resetCamZero(delta):
+	if not ResetCam: return
+	CamPivot.rotation.y = lerp(CamPivot.rotation.y, abs(0.0 + VisualNode.rotation.y), 0.1 * delta)
+
 #---- Main Functions
 
 func _ready():
-	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	MouseLocked = true
-
+	help_mouseMode()
 
 func _input(event):
 	if event.is_action_pressed("Sprint") and not ShiftToggleRun and not CurrentState == 5:
@@ -87,19 +115,54 @@ func _input(event):
 		else:
 			Sprinting = false
 	
+	if event.is_action_pressed("Menu"):
+		InUI = not InUI
+		if InUI:
+			MouseLocked = false
+		else:
+			MouseLocked = MouseLockViaShortcut
+		help_mouseMode()
+	
+	if event.is_action_pressed("MouseLock"):
+		MouseLockViaShortcut = not MouseLockViaShortcut
+		MouseLocked = MouseLockViaShortcut
+		help_mouseMode()
+	
+	
+	
+	#-- Mouse Inputs Below
 	if event is InputEventMouseMotion and MouseLocked:
 		x_rot -= event.relative.x * sens
 		y_rot -= event.relative.y  * sens
 		
-		y_rot = clamp(y_rot, -80, 60)
+		y_rot = clamp(y_rot, -88, 88)
 		CamPivot.rotation.x = deg_to_rad(y_rot)
 		CamPivot.rotation.y = deg_to_rad(x_rot)
+		
+		ResetCam = false
+		ResetCamTimer.start(1.8)
+		
+	
+	if event.is_action_pressed("CameraZoomIn"):
+		var zoom = CamSpring.spring_length
+		ZoomPressure = ZoomPressure * 2
+		var new_zoom = zoom - (0.01 + ZoomPressure)
+		CamSpring.spring_length = clamp(new_zoom, zoomMin, zoomMax)
+	
+	if event.is_action_pressed("CameraZoomOut"):
+		var zoom = CamSpring.spring_length
+		ZoomPressure = ZoomPressure * 2
+		var new_zoom = zoom + (0.01 + ZoomPressure)
+		CamSpring.spring_length = clamp(new_zoom, zoomMin, zoomMax)
+	
+	if event.is_action_released("CameraZoomIn") or event.is_action_released("CameraZoomOut"):
+		ZoomPressure = 0.01
+	
 
 func _physics_process(delta):
 	var input_dir = Input.get_vector("MoveLeft", "MoveRight", "MoveFront", "MoveBack")
 	
 	# Check for State Conversion
-	
 	CurrentState = 9 # Buffer Current State
 	
 	if input_dir:
@@ -115,7 +178,6 @@ func _physics_process(delta):
 		CurrentState = 5
 	
 
-
 	# State Machine
 	var State = States[CurrentState]
 	match State:
@@ -125,20 +187,20 @@ func _physics_process(delta):
 		
 		"Walking":
 			Speed = WalkSpeed
+			resetCamZero(delta)
 		
 		"Running":
 			Speed = RunSpeed
+			resetCamZero(delta)
 		
 		"Jumping":
 			velocity.y = JumpVel
-			print("Jump")
 		
 		"Climbing":
 			pass
 		
 		"Falling":
 			velocity += get_gravity() * delta
-			print("Fall")
 	
 
 	# Velocity
@@ -160,3 +222,7 @@ func _Knockback(knockbackVelocity, knockbackDecel):
 func Idle():
 	CurrentState = 0
 	start_idleTimer()
+
+
+func ResetCamRotation():
+	ResetCam = true
